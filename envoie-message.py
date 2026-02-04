61ad9b92f01fa059434cc040d0f5bc1194fd9d32
tortinouminou-svg <tortinou.minou@gmail.com>
2026-02-04 22:40:54 +0100

    Update envoie-message.py

--- envoie-message.py
+++ envoie-message.py
@@ -2,6 +2,16 @@ import requests
 import csv
 import io
 import os
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+ALLOWED_HOURS = {0, 4, 8, 12, 16, 20}
+
+now_fr = datetime.now(ZoneInfo("Europe/Paris"))
+
+if now_fr.minute != 5 or now_fr.hour not in ALLOWED_HOURS:
+    print("⏭️ Pas le bon créneau")
+    exit(0)
 
 DISCORD_WEBHOOK = os.getenv("DISCORD_WEBHOOK")
 
